48295e175f3aea9ada3fd716330335d42861677b
mround <mround@mathworks.com>
2020-12-25 12:02:23 +0100

    Still trying to make CI build.

--- tst/multinomial_test.cpp
+++ tst/multinomial_test.cpp
@@ -9,6 +9,7 @@ class MultinomialTest : public testing::Test
 public:
   multinomial P, Q;
   symbol a, b, x, y, z;
+  std::vector<symbol> P_vars, sorted_P_vars, Q_vars;
 
   virtual void SetUp()
   {
@@ -19,8 +20,11 @@ public:
     z = symbol("z");
     // create a multinomial and make the constructor sort the variables into
     // lexicographical ordering
-    P = multinomial(pow(x, 3) + pow(y, 4) + x*y, std::vector<symbol>({z, x, b, y, a}));
-    Q = multinomial(pow(x, y), std::vector<symbol>({z}));
+    P_vars = {z, x, b, y, a};
+    sorted_P_vars = {a,b,x,y,z};
+    Q_vars = {z};
+    P = multinomial(pow(x, 3) + pow(y, 4) + x*y, P_vars);
+    Q = multinomial(pow(x, y), Q_vars);
   }
 
   virtual void TearDown()
@@ -30,7 +34,7 @@ public:
 
 TEST_F(MultinomialTest, testConstructor)
 {
-  EXPECT_EQ(std::vector<symbol>({a, b, x, y, z}), P.vars());
+  EXPECT_EQ(sorted_P_vars, P.vars());
   EXPECT_EQ(pow(x,3) + pow(y,4) + x*y, P.expr());
 }
 
