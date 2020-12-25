8bc5952d37260a7c30857232ef03b65b8fe6cc1a
mround <mround@mathworks.com>
2020-12-25 14:35:22 +0100

    Correct error testing.

--- tst/multinomial_test.cpp
+++ tst/multinomial_test.cpp
@@ -22,7 +22,6 @@ public:
     // create a multinomial and make the constructor sort the variables into
     // lexicographical ordering
     P = multinomial(pow(x, 3) + pow(y, 4) + x*y, P_vars);
-    Q = multinomial(pow(x, y), Q_vars);
   }
 
   virtual void TearDown()
@@ -34,6 +33,7 @@ TEST_F(MultinomialTest, testConstructor)
 {
   EXPECT_EQ(sorted_P_vars, P.vars());
   EXPECT_EQ(pow(x,3) + pow(y,4) + x*y, P.expr());
+  EXPECT_THROW(multinomial(pow(x, y), Q_vars), std::domain_error);
 }
 
 int main(int argc, char **argv) {
