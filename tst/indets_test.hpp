690c567645806260f5d2617ce56a67c0de521d54
mround <mark.s.round@gmail.com>
2021-01-05 01:54:59 +0100

    Try to fix Travis.

--- tst/indets_test.hpp
+++ tst/indets_test.hpp
@@ -6,15 +6,17 @@ class IndetsTest : public testing::Test
 {
 public:
   multinomial P1, P2;
-  indet_lst I1, I2;
+  indet_lst I1, I2, I1res, I2res;
   indeterminate x, y, z;
   virtual void SetUp()
   {
-    P1 = pow(x, 2) - y;
-    P2 = pow(x, 3) - x + pow(x+y+z, 4);
+    P1 = x*x - y;
+    P2 = x*x*x - x + pow(x+y+z, 4);
 
     I1 = indets(P1);
     I2 = indets(P2);
+    I1res = {x, y};
+    I2res = {x, y, z};
   }
 
   virtual void TearDown()
@@ -24,6 +26,6 @@ public:
 
 TEST_F(IndetsTest, testIndets)
 { // GiNaC's sort only gaurantees an order at runtime
-  EXPECT_EQ((GiNaC::lst({x, y})).sort(), I1);
-  EXPECT_EQ((GiNaC::lst({x, y, z})).sort(), I2);
+  EXPECT_EQ(I1res.sort(), I1);
+  EXPECT_EQ(I2res.sort(), I2);
 }
